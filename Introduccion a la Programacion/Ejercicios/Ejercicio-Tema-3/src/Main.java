4f889ac0b1bba678d054ce25e560a0c5e4e2890d
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2022-12-05 14:34:30 -0300

    addign first part of exercise theme 3

--- Introduccion a la Programacion/Ejercicios/Ejercicio-Tema-3/src/Main.java
+++ Introduccion a la Programacion/Ejercicios/Ejercicio-Tema-3/src/Main.java
@@ -3,6 +3,13 @@ public class Main {
         Car myCar = new Car();
         myCar.AddDoors();
         System.out.println(myCar.doors);
+
+        var result = suma(10,20,5);
+        System.out.println(result);
+    }
+
+    public static int suma(int a, int b, int c){
+       return a+b+c;
     }
 
 
