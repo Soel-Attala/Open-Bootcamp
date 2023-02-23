f99e7d87c3b12f33131de13cfab5722bc9a7c6e0
Soel Attala <soelattala7@gmail.com>
2023-02-23 13:42:14 -0300

    agregamos funciones del modulo 4

--- SQL/sesion4-consultas.sql
+++ SQL/sesion4-consultas.sql
@@ -1,291 +1,123 @@
-/* BASE DE DATOS PAGILA */
+SELECT * FROM address;
+-- nos da 604 resultados
+SELECT district FROM address;
+--nos devuelve 604 resultados por que hay 604 distritos
+-- habra varios distritos repetidos, si queremos que nos de todos diferentes para saber exactamente el total sería:
+SELECT DISTINCT district FROM address;
+--con esto obtenemos 379 resultados ya que quitamos los duplicados
 
-/*
- distinct
-*/
+SELECT last_name FROM customer;
+--nos da 600 resultados
 
--- 604 resultados
-select * from address;
-
--- 604 resultados
-select district from address;
-
--- obtener distritos únicos 379 resultados
-select distinct district from address;
-
--- 600 resultados
-select first_name from customer;
-
--- 592 resultados
-select distinct first_name from customer;
+SELECT DISTINCT last_name FROM customer;
+--nos da 600 tambien, ya que no hay repetidos
 
 
-/*
- and, or, not
- order by
- 
- and: se tienen que cumplir si o si las condiciones
- or: con que se cumpla una condición es suficiente
- not: niega una condición
-*/
-select * from address where district = 'California';
-select * from address where district != 'California';
-select * from address where not district = 'California';
-select * from address where not district = 'California' order by district;
+-- and, or, not
+SELECT * FROM address WHERE district = 'California';
+SELECT * FROM address WHERE district != 'California';
+SELECT * FROM address WHERE NOT district = 'California' ORDER BY district;
+-- el comando con WHERE NOT es lo mismo que != es decir el segundo y el tercero son los mismo
+-- con order by seleccionamos una columna y hacemos que nos ordene los datos de la tabla en 
+--orden alfabetico segun esa columna
+SELECT * FROM address WHERE district = 'California' OR district = 'Abu Dhabi' ORDER BY district;
+--con el comando or, le pedimos que nos traiga los datos que cumplan algunas de las condiciones.
+SELECT * FROM address WHERE district IS NOT NULL AND district = '' ORDER BY district;
+-- de esta manera le pedimos que no traiga ni los nulos, ni los que tienen texto vacio.
 
-select * from address where district = 'Abu Dhabi' or district = 'California';
+SELECT * FROM address WHERE NOT district = '' ORDER BY district;
+SELECT * FROM address WHERE district IS NOT NULL ORDER BY district;
 
-select * from address where district is not null order by district;
-select * from address where not district = '' order by district;
+SELECT * FROM address WHERE address2 IS NOT NULL AND address_id = 1 ORDER BY district;
 
-select * from address where address2 is not null and address_id = 1 order by district;
 
-/*
-	group by
-*/
 
-select address_id, district from address;
-select district, count(district) from address group by district;
-select district, count(district) from address group by district order by district;
-select district, count(district) as veces  from address group by district order by district;
+-- group by
+--nos pedira que usemos ciertos datos en una funcion de agregacion como condicion,
+--una funcion de agregacion es una funcion que reduce los datos, es decir, por ejemplo hace un calculo.
+
+SELECT district, COUNT (district) FROM address group by district;
+-- aqui le pedimos que nos de un distrito y luego la cuenta.
+SELECT district, COUNT (district) as repeat FROM address group by district;
+-- con el comando as solo ponemos el nombre de la columna del count.
+
+
+
+-- UNION 
+-- si tenemos dos tablas iguales, supongamos que una tiene datos del 2020 y la otra del 2021
+-- con union podemos traer los datos de ambas combinados en una sola tabla.
+
+
+--joins 
+SELECT * FROM customer;
+SELECT * FROM address;
+SELECT * FROM city;
+SELECT * FROM country;
+-- estas tabalas estan relacionadas, es decir supongamos que queremos enviar un producto a un cliente
+-- entonces debemos obtener info de todas las tablas relacionadas.
+
+-- consultaremos dos tablas, customer y address
+SELECT first_name, last_name, address_id FROM customer 
+INNER JOIN address on customer.address_id = address.address_id;
+--nos dara error
+
+SELECT first_name, last_name, customer.address_id FROM customer 
+INNER JOIN address on customer.address_id = address.address_id;
+
+--tenemos distintos tipos de join, ya sea right y left que hace que se incluyan los datos de las tablas
+-- de derecha o izquierda aunque no coincidan.
+--colocamos customer en el campo de arriba para resolver la ambiguedad por que ambos campos tienen el mismo nombre
+-- de esta forma le indicamos que pertenece a customer.
+
+
+--con el inner sacamos solo los que coinciden con los que estamos pidiendo
+
+--cuando hacemos el inner debemos indicar la columna que se relaciona, entonces, vemos que address_id
+-- es la clave foranea de customer y la primaria de address,
+--por lo que utilizamos dicha columna planteando su relacion.
+
+--podemos abreviar esto asi
+SELECT first_name, last_name, customer.address_id FROM customer 
+INNER JOIN address on customer.address_id = address.address_id;
+
+SELECT first_name, last_name, c.address_id FROM customer c
+INNER JOIN address a on c.address_id = a.address_id;
+-- vemos que es lo mismo, lo unico que hacemos es asignarle la letra c a customer y a address la letra a.
+
+--agregaremos la direccion a la tabla
+SELECT first_name, last_name, customer.address_id, address FROM customer 
+INNER JOIN address on customer.address_id = address.address_id;
+-- aqui hemos reunido los datos de ambas tablas por lo que podemos traer los datos de ambas.
+-- si hacemos esto:
+SELECT * FROM customer 
+INNER JOIN address on customer.address_id = address.address_id;
+--nos trae todos los datos de ambas tablas, una tabla al lado de otra, es muy poco practico.
+
+
+SELECT c.email, a.address FROM customer c
+INNER JOIN address a on c.address_id = a.address_id;
+
+
+-- CONSULTA A 3 TABLAS: CUSTOMER, ADDRESS, CITY.
+SELECT cu.email, a.address, ci.city FROM customer cu
+INNER JOIN address a on cu.address_id = a.address_id
+INNER JOIN city ci ON a.city_id = ci.city_id;
+
+--basicamente aqui repetimos lo anterior, relacionamos customer -> address (en el primer inner join),
+-- y address con city (en el segundo inner join).
+
+
+-- CONSULTA A 4 TABLAS:
+SELECT cu.email, a.address, ci.city, co.country FROM customer cu
+INNER JOIN address a on cu.address_id = a.address_id
+INNER JOIN city ci ON a.city_id = ci.city_id
+INNER JOIN country co ON ci.country_id = co.country_id ORDER BY country;
+
+
+--FUNCION CONCATENAR: UNIREMOS DOS COLUMNAS DE UNA TABLA EN UNA NUEVA.
 
 SELECT * FROM actor;
-SELECT last_name, count(last_name) from actor group by last_name;
-SELECT last_name, count(last_name) from actor group by last_name HAVING count(last_name) > 1;
 
--- obtener en cuantas películas actúa cada actor:
-select * from film_actor;
-select * from film;
+SELECT CONCAT(first_name,'  ', last_name) as Full_Name from actor;
 
-select f.title, count(fa.actor_id) from film f
-inner join film_actor fa on f.film_id = fa.film_id
-group by f.title
-
--- stock de una película en base a su título
-select * from inventory;
-
-select f.title, count(i.inventory_id) as unidades from film f
-inner join inventory i on i.film_id = f.film_id
-GROUP BY title;
-
-select f.title, count(i.inventory_id) as unidades from film f
-inner join inventory i on i.film_id = f.film_id
-WHERE title = 'FICTION CHRISTMAS'
-GROUP BY title;
-
-select f.title, count(i.inventory_id) as unidades from film f
-inner join inventory i on i.film_id = f.film_id
-GROUP BY title ORDER BY unidades;
-
-select f.title, count(i.inventory_id) as unidades from film f
-inner join inventory i on i.film_id = f.film_id
-GROUP BY title ORDER BY unidades DESC;
-
-/*
- SUM()
- */
- select * from customer;
-  select * from payment;
-
-SELECT * FROM payment p
-inner join customer c on p.customer_id = c.customer_id
-
-SELECT c.email, count(p.payment_id) as num_pagos FROM payment p
-inner join customer c on p.customer_id = c.customer_id
-group by c.email
-
-SELECT c.email, sum(p.amount) as num_pagos FROM payment p
-inner join customer c on p.customer_id = c.customer_id
-group by c.email
-
-select * from staff;
-
-select * from payment p
-inner join staff s on p.staff_id = s.staff_id
-
-select s.first_name, count(p.payment_id) as num_ventas, sum(p.amount) cantidad_ventas from payment p
-inner join staff s on p.staff_id = s.staff_id
-group by s.first_name
-
-
--- joins
-
-select * from customer;
-select * from address;
-select * from city;
-select * from country;
-
--- consulta a 2 tablas: customer y address
-
-select first_name, last_name, customer.address_id from customer 
-inner join address on customer.address_id = address.address_id
-
-select * from customer c
-inner join address a on c.address_id = a.address_id
-
-select c.email, a.address from customer c
-inner join address a on c.address_id = a.address_id
-
--- consulta a 3 tablas: customer, address, city
-SELECT * FROM customer cu
-INNER JOIN address a ON cu.address_id = a.address_id
-INNER JOIN city ci ON a.city_id = ci.city_id
-
-SELECT cu.email, a.address, ci.city  FROM customer cu
-INNER JOIN address a ON cu.address_id = a.address_id
-INNER JOIN city ci ON a.city_id = ci.city_id
-
--- consulta a 4 tablas: customer, address, city, country
-SELECT *  FROM customer cu
-INNER JOIN address a ON cu.address_id = a.address_id
-INNER JOIN city ci ON a.city_id = ci.city_id
-INNER JOIN country co ON ci.country_id = co.country_id
-
-SELECT cu.email, a.address, ci.city, co.country  FROM customer cu
-INNER JOIN address a ON cu.address_id = a.address_id
-INNER JOIN city ci ON a.city_id = ci.city_id
-INNER JOIN country co ON ci.country_id = co.country_id
-
-
-/*
- Función concat()
-*/
-select * from actor;
-
-select first_name, last_name from actor;
-
-select concat(first_name, ' ', last_name) from actor;
-
-select concat(first_name, ' ', last_name) as full_name from actor;
-
-/*
- LIKE
-*/
-select * from film;
-
-SELECT * from film WHERE description LIKE '%Monastery';
-SELECT * from film WHERE description LIKE '%Drama%';
-
-select * from actor
-
-select * from actor where last_name like '%LI%';
--- Orden ascendente, empieza por el principio y va hasta el final
-select * from actor where last_name like '%LI%' order by last_name;
--- Orden descendente, empieza por el final y va hasta el principio
-select * from actor where last_name like '%LI%' order by last_name DESC;
-
-
-/*
- IN
-*/
-
-select * from country;
-
-select * from country where country = 'Spain';
-select * from country where country = 'Spain' or country = 'Germany';
-select * from country where country = 'Spain' or country = 'Germany' or country = 'France';
-
-SELECT * FROM country WHERE country IN('Spain', 'Germany', 'France', 'Mexico');
-
-select * from customer;
-
-SELECT * FROM customer WHERE customer_id = 15;
-
-SELECT * FROM customer WHERE customer_id IN(15, 16, 17, 18);
-
-
-/*
- Sub queries
-*/
-
-select * from film;
-select * from language;
-
-select distinct language_id from film;
-
-select * from film f
-inner join language l on f.language_id = l.language_id 
-
-select l.name, count(f.film_id) from film f
-inner join language l on f.language_id = l.language_id 
-group by l.name
-
--- Cambiar idioma a algunas películas
-UPDATE film SET language_id = 2 WHERE film_id > 100 and film_id < 200;
-UPDATE film SET language_id = 3 WHERE film_id >= 200 and film_id < 300;
-UPDATE film SET language_id = 4 WHERE film_id >= 300 and film_id < 400;
-
-SELECT title from film
-where language_id = (SELECT language_id FROM language WHERE name = 'English')
-
-SELECT title from film
-where language_id IN (SELECT language_id FROM language WHERE name = 'English' or name = 'Italian')
-
--- peliculas más alquiladas
-
-select * from rental;
-select * from inventory;
-select * from film;
-
-
-SELECT * from film f
-inner join (select * from inventory i
-inner join rental r on r.inventory_id = i.inventory_id) res on res.film_id = f.film_id
- 
-SELECT f.title, count(f.film_id) as veces_alquilada from film f
-inner join (select * from inventory i
-inner join rental r on r.inventory_id = i.inventory_id) res on res.film_id = f.film_id
-group by f.title
-order by veces_alquilada DESC
-
-
-/* BASE DE DATOS concesionario */
-
-select * from customer;
-select * from employee;
-select * from extra;
-select * from extra_version;
-select * from manufacturer;
-select * from model;
-select * from sale;
-select * from vehicle;
-select * from version;
-
--- count ventas por empleado
-
-INSERT INTO sale(sale_date, channel, id_vehicle, id_employee, id_customer)
-VALUES('2022-01-01', 'Phone', 1, 1, 1);
-
-select * from sale s
-inner join employee e on s.id_employee = e.id
-
-select e.name, count(s.id) from sale s
-inner join employee e on s.id_employee = e.id
-group by e.name
-
--- count compras por cliente
-select c.email, count(s.id) from sale s
-inner join customer c on s.id_customer = c.id
-group by c.email
-
--- fabricante mas vendido
-select * from sale;
-select * from vehicle;
-select * from manufacturer;
-
-select * from sale s
-inner join vehicle v on s.id_vehicle = v.id
-inner join manufacturer m on v.id_manufacturer = m.id
-
-select m.name, count(s.id) from sale s
-inner join vehicle v on s.id_vehicle = v.id
-inner join manufacturer m on v.id_manufacturer = m.id
-group by m.name
-
--- modelo mas vendido
-
--- version mas vendido
-
--- extra vendido
-
--- ventas agrupando por año, mes, dia
+-- Funcion Like:
