83503f556c92d9982f0c5e30a1ad024c30bc4cd7
Soel Attala <soelattala7@gmail.com>
2023-03-08 20:01:11 -0300

    agregamos clase 2

---
+++ Curso-C#/Clases/Clase-1/Clase 2-2.cs
@@ -0,0 +1,49 @@
+//OPERADORES
+//Operadores aritméticos
+// + - * / % ++ --
+int i = 0 ;
+Console.WriteLine("Suma: "+(i+1));
+Console.WriteLine("Autoincremento: "+ i++);
+Console.WriteLine("Division: "+(i/2));
+//con la division si el resultado es decimal, nos devuelve el entero mas cercano, por ejemplo 5/2 = 2
+//si es 0.5 nos devuelve 0, si es 1.5 nos devuelve 1, etc.
+Console.WriteLine("Autodecremento: "+ --i);
+Console.WriteLine("Resta: "+(i-1));
+Console.WriteLine("Modulo: "+(i%2));
+//modulo nos devuelve el resto de la division, por ejemplo 5%2 = 1
+
+
+
+//Operadores de asignación
+//son operadores que nos permiten asignar un valor a una variable
+// = += -= *= /= %= <<= >>= &= ^= |=
+int i = 0;
+
+
+
+
+//Operadores de comparación
+// == != > < >= <=
+//los operadores de comparacion nos devuelven un valor booleano, es decir, true o false.
+
+//Operadores lógicos
+// && || !
+
+//Ejemplos:
+// && = and
+Console.WriteLine("Operador &&: "+(true && true));
+// || = or
+Console.WriteLine("Operador ||: "+(true || false));
+// ! = not
+Console.WriteLine("Operador !: "+(!true));
+//los operadores logicos nos devuelven un valor booleano, es decir, true o false.
+
+
+
+
+
+
+//Operadores de incremento y decremento
+//Operadores de igualdad
+//Operadores de desplazamiento
+//Operadores de bits
