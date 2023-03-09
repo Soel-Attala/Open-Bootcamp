a8331afa1580a05df48fded07b2d97285c396ca4
Soel Attala <soelattala7@gmail.com>
2023-03-09 14:11:02 -0300

    agregamos clase 3 con su ejercicio

---
+++ Curso-C#/Clases/Clase-3/Program.cs
@@ -0,0 +1,54 @@
+﻿//CLASE 3: ESTRUCTURAS DE DATOS
+
+//Los datos del mismo tipo se pueden almacenar en colecciones
+//podemos usar metodos de colecciones para realizar operacionesd de forma mas simple 
+//y evitar errores.
+
+//ARRAYS
+//Tenemos arrays unidimencionales, bidimencionales, y tridimencionales.
+// para localizar los datos en cada uno necesitamos 1,2,3 indices respectivamente.
+// los indices representan la posicion, partiendo desde el cero
+
+using System.Security.Cryptography.X509Certificates;
+
+Coords myCoords = new Coords(0, 1, 3);
+Console.WriteLine(myCoords.X);
+Console.WriteLine(myCoords.Y);
+Console.WriteLine(myCoords.Z);
+Coords myCoords2 = myCoords with { X = 3 };
+Console.WriteLine(myCoords2);
+
+public struct Coords
+{
+    public Coords(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    public double X { get; set; }
+    public double Y { get; set; }
+    //si dejamos solo get es solo lectura, si agregamos set nos deja agregar escritura
+    public double Z { get; set; }
+
+
+    public override string ToString() => $"({X}, {Y}, {Z})";
+
+}
+
+/*
+arriba haremos un objeto con esta estructura,
+lo hacemos arriba para que no tire error 
+
+en la consola podremos ver los valores de x e y asignados mas arriba.
+
+procedemos a crear la letra z y hacerlo tridimencional.
+
+
+podemos crear un objeto de tipo referencia, cada objeto tiene un tipo definido.
+podemos crear objetos de lectura, usando readOnly, ejemplo public readonly struct Coords
+el read only basicamente deja solo leer por lo que solo se pueden asignar datos al crearlo,
+despues no puede modificarse.
+*/
+
+
---
+++ Curso-C#/Trabajos Practicos/Ejercicio-3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * EJERCICIO 3:
+ * Crea una estructura de datos para un cliente con estos campos:
+ * Nombre completo
+ * Teléfono
+ * Dirección
+ * Email
+ * Si es nuevo cliente
+ * Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+ */
+
+Data data = new Data("Soel Antonio Attala", 306032, "El Moro 315", "soelattala7@gmail.com", "yes");
+
+Data data2 = data with { Name = "Luciano", Phone = 504520, Address = "Los Filtros 775", Email = "luciano123@gmail.com", Newclient = "yes" };
+void ShowData()
+{
+    if (data.Name != null)
+    {
+        Console.WriteLine("PrimerCliente: " + data);
+        Console.WriteLine("Segundo Cliente" + data2);
+    }
+}
+
+ShowData();
+
+public struct Data
+{
+    public Data(string name, int phone, string address, string email, string newclient)
+    {
+        Name = name;
+        Phone = phone;
+        Address = address;
+        Email = email;
+        Newclient = newclient;
+    }
+    public string Name { get; set; }
+    public int Phone { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Newclient { get; set; }
+
+    public override string ToString() => $"({Name}, {Phone}, {Address}, {Email}, {Newclient})";
+
+
+}
