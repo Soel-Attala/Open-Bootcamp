6ffa8ea59349d5e1de24687e8b016a2b1cc68052
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2023-04-15 16:00:33 -0300

    We add class 11

---
+++ Curso-C#/Clases/Clase-11/Generics.cs
@@ -0,0 +1,26 @@
+﻿//GENERICS, HERENCIA, POLIMORFIMO, SOBRECARGA DE OPERADORES, INTERFACES
+
+//GENERICS: los tipos genericos permiten personalizar un metodo, una clase, estructura o interfaz. En estas estructuras, podemos
+// incluir elementos genericos en los cuales podemos organizar nuestros datos.
+// los genericos tienen marcadores de posicion que tienen parametros de tipos, para uno o varios tipos que almacenan su contenido.
+// podemos guardar informacion en estos genericos y acceder a ellos.
+
+//Ejemplo:
+
+//Generics
+/*
+Generic<string> str = new Generic<string>();
+str.Value = "value";
+Console.WriteLine($"Generic.Value: {str.Value}");
+Console.WriteLine( $"Tipo Generic de dato: {str.Value.GetType()}");
+Console.WriteLine($"Tipo Generic de dato{str.GetType()}");
+public class Generic<T>
+{
+    public T Value { get; set; }
+    public int myInt { get; set; }
+
+}
+*/
+
+
+
