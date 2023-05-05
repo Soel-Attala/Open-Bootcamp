6aae450ad4ff5b2ed70896387d5d9833cd7649b1
Soel Attala <soelattala7@gmail.com>
2023-05-05 02:34:02 -0300

    we adding class 20 - Debug

---
+++ Curso-C#/Clases/Clase-20/Program.cs
@@ -0,0 +1,12 @@
+﻿//Debugging - Debug (Depuracion)
+
+Console.WriteLine("Write your message");
+var msg = Console.ReadLine();
+Console.WriteLine($"I've recived this message: {msg}");
+int resultado = 0;
+for (int i = 0; i < 10; i++)
+{
+    resultado = (i * i) + 1;
+    Console.WriteLine(resultado);
+}
+Console.ReadLine();
