d991e3b60a108fc38eddb56740a2cc40fd864dee
Soel Attala <soelattala7@gmail.com>
2023-05-22 16:27:25 -0300

    we add the second session

---
+++ .NET/Sessions/MyFirstAPI/DataAccess/UniversityContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options)
+        {
+
+        }
+        //TODO: add DBSets (tables of our database)
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
