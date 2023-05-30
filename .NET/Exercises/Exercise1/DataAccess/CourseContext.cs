31a8bf107d0310600248436796490b3f03b8f59e
Soel Attala <soelattala7@gmail.com>
2023-05-30 14:19:24 -0300

    We add exercise 1 - Course API

---
+++ .NET/Exercises/Exercise1/DataAccess/CourseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CourseBackend.Models.DataModels;
+namespace CourseBackend.DataAccess
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(DbContextOptions<CourseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+
+}
---
+++ .NET/Exercises/Exercise1/Models/DataModels/Course.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CourseBackend.Models.DataModels
+{
+    public enum CourseLevel
+    {
+        Basico,
+        Intermedio,
+        Avanzado
+    }
+    public class Course : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+        [Required, StringLength(600)]
+        public string LongDescription { get; set; } = string.Empty;
+        [Required, StringLength(20)]
+        public string TargetAudience { get; set; } = string.Empty;
+        [Required, StringLength(100)]
+        public string Objectives { get; set; } = string.Empty;
+        [Required, StringLength(100)]
+        public string Requirements { get; set; } = string.Empty;
+        [Required]
+        public CourseLevel Level { get; set; }
+
+    }
+}
