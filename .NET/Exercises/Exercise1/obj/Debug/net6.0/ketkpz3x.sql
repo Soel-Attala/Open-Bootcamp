f44c3753ecfa2c135f92d3821a4ca26d6201dd02
Soel Attala <soelattala7@gmail.com>
2023-05-30 14:33:44 -0300

    We execute the migration and update the database

---
+++ .NET/Exercises/Exercise1/obj/Debug/net6.0/ketkpz3x.sql
@@ -0,0 +1,39 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Courses] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+    [ShortDescription] nvarchar(280) NOT NULL,
+    [LongDescription] nvarchar(600) NOT NULL,
+    [TargetAudience] nvarchar(20) NOT NULL,
+    [Objectives] nvarchar(100) NOT NULL,
+    [Requirements] nvarchar(100) NOT NULL,
+    [Level] int NOT NULL,
+    [CreatedBy] nvarchar(max) NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(max) NOT NULL,
+    [UpdateAt] datetime2 NULL,
+    [DeletableBy] nvarchar(max) NOT NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230530173050_Create Courses Table', N'6.0.16');
+GO
+
+COMMIT;
+GO
+
